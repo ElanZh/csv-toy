6a3f1a50e0e0d60e1030dcc3d9e4126bf753acb5
zhangyiran <zhangyr@sunyard.com>
2019-06-06 17:19:13 +0800

    读取yml配置文件实现

--- src/main/java/elan/toy/csv/Execute.java
+++ src/main/java/elan/toy/csv/Execute.java
@@ -1,5 +1,11 @@
 package elan.toy.csv;
 
+import elan.toy.csv.config.SysConfig;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+
 /**
  * @author 张一然
  * @date 2019/6/6 16:01
@@ -8,8 +14,12 @@ package elan.toy.csv;
  */
 public class Execute {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 //        System.out.println("hello world!");
         // 读取yml文件
+        Yaml yaml = new Yaml();
+        SysConfig sysConfig = yaml.loadAs(new FileInputStream(new File("./application.yml")), SysConfig.class);
+
+        System.out.println(sysConfig.getFile().getReadInFile());
     }
 }
---
+++ src/main/java/elan/toy/csv/config/SysConfig.java
@@ -0,0 +1,12 @@
+package elan.toy.csv.config;
+
+/**
+ * @author 张一然
+ * @date 2019/6/6 17:16
+ * @Package elan.toy.csv.config
+ * @Description
+ */
+@lombok.Data
+public class SysConfig {
+    private FileConfig file;
+}
