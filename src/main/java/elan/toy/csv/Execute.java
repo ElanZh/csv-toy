87db37eebfc2000d890b8c363549b20bb3fd32b9
zhangyiran <zhangyr@sunyard.com>
2019-06-06 17:04:52 +0800

    初始化

---
+++ src/main/java/elan/toy/csv/Execute.java
@@ -0,0 +1,15 @@
+package elan.toy.csv;
+
+/**
+ * @author 张一然
+ * @date 2019/6/6 16:01
+ * @Package elan.toy.csv
+ * @Description
+ */
+public class Execute {
+
+    public static void main(String[] args){
+//        System.out.println("hello world!");
+        // 读取yml文件
+    }
+}
---
+++ src/main/java/elan/toy/csv/config/FileConfig.java
@@ -0,0 +1,14 @@
+package elan.toy.csv.config;
+
+
+/**
+ * @author 张一然
+ * @date 2019/6/6 16:21
+ * @Package elan.toy.csv.config
+ * @Description
+ */
+@lombok.Data
+public class FileConfig {
+    private String readInFile;
+    private String writeOutFile;
+}
