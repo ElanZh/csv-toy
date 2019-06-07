3fe1dc87f2bf62aa49a46b700304b0fe2f5bae97
ElanZh <elan_zh@163.com>
2019-06-08 00:16:50 +0800

    opencsv

--- src/main/java/elan/toy/csv/biz/CsvBiz.java
+++ src/main/java/elan/toy/csv/biz/CsvBiz.java
@@ -2,7 +2,9 @@ package elan.toy.csv.biz;
 
 import elan.toy.csv.config.SysConfig;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /**
  * @author 张一然
@@ -13,6 +15,9 @@ import java.io.FileInputStream;
 public class CsvBiz {
 
     public static void exec(FileInputStream fileIn, SysConfig sysConfig) throws Exception {
+//        List<User> userList = new CsvToBeanBuilder<User>(new BufferedReader(
+//                new InputStreamReader(fileIn, "GB2312")
+//        )).withType(User.class).build().parse();
         // 读取完毕关闭IO
         fileIn.close();
     }
